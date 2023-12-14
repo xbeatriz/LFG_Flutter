ac5cdbd54152719865c410159b554c167183adad
Beatriz <120328207+xbeatriz@users.noreply.github.com>
2023-12-14 14:19:33 +0000

    EventCard

--- lib/pages/widgets/eventCard.dart
+++ lib/pages/widgets/eventCard.dart
@@ -1,143 +1,5 @@
 import 'package:flutter/material.dart';
 
-class MyEvents extends StatefulWidget {
-  const MyEvents({Key? key}) : super(key: key);
-
-  @override
-  State<MyEvents> createState() => _MyEventsState();
-}
-
-class _MyEventsState extends State<MyEvents> {
-  String selectedCategory = 'Favorites';
-
-  List<EventData> events = [
-    EventData(
-      imageUrl: 'assets/images/Valorant.png',
-      eventTitle: 'TÍTULO DO EVENTO 1',
-      userName: 'NOME DO USUÁRIO 1',
-      dateAndTime: 'DATA E HORA 1',
-    ),
-    EventData(
-      imageUrl: 'assets/images/Valorant.png',
-      eventTitle: 'TÍTULO DO EVENTO 2',
-      userName: 'NOME DO USUÁRIO 2',
-      dateAndTime: 'DATA E HORA 2',
-    ),
-    // Adicione mais eventos conforme necessário
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Os meus eventos"),
-        backgroundColor: Color(0xFF000B45),
-        automaticallyImplyLeading: false,
-      ),
-      body: Column(
-        children: [
-          SizedBox(height: 8.0),
-          Container(
-            height: 47,
-            width: 390,
-            decoration: BoxDecoration(
-              color: Colors.grey,
-              borderRadius: BorderRadius.circular(8.0),
-            ),
-            child: Row(
-              children: [
-                Expanded(
-                  child: GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        selectedCategory = 'Favorites';
-                      });
-                    },
-                    child: Container(
-                      height: double.infinity,
-                      decoration: BoxDecoration(
-                        color: selectedCategory == 'Favorites'
-                            ? Color(0xFF000B45)
-                            : Colors.transparent,
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(8.0),
-                          bottomLeft: Radius.circular(8.0),
-                        ),
-                      ),
-                      child: Center(
-                        child: Text(
-                          "Favorites",
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        selectedCategory = 'Subscribed';
-                      });
-                    },
-                    child: Container(
-                      height: double.infinity,
-                      decoration: BoxDecoration(
-                        color: selectedCategory == 'Subscribed'
-                            ? Color(0xFF000B45)
-                            : Colors.transparent,
-                        borderRadius: BorderRadius.only(
-                          topRight: Radius.circular(8.0),
-                          bottomRight: Radius.circular(8.0),
-                        ),
-                      ),
-                      child: Center(
-                        child: Text(
-                          'Subscribed',
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          SizedBox(height: 8.0),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              ListView.builder(
-                shrinkWrap: true,
-                itemCount: events.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Padding(
-                    padding: const EdgeInsets.only(bottom: 8.0),
-                    child: EventCard(
-                      imageUrl: events[index].imageUrl,
-                      eventTitle: events[index].eventTitle,
-                      userName: events[index].userName,
-                      dateAndTime: events[index].dateAndTime,
-                      discordAccount: '',
-                      gameName: '',
-                      ageLimit: '',
-                      description: '',
-                    ),
-                  );
-                },
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-}
-
 class EventCard extends StatelessWidget {
   final String imageUrl;
   final String eventTitle;
@@ -165,61 +27,73 @@ class EventCard extends StatelessWidget {
       onTap: () {
         _showEventDetails(context);
       },
-      child: Container(
-        height: 130,
-        margin: EdgeInsets.symmetric(horizontal: 16.0),
-        padding: EdgeInsets.all(16.0),
-        decoration: BoxDecoration(
-          color: Color(0xFF000B45),
-          borderRadius: BorderRadius.circular(16.0),
-          boxShadow: [
-            BoxShadow(
-              color: Color.fromARGB(255, 0, 0, 0).withOpacity(0.2),
-              spreadRadius: 5,
-              blurRadius: 2,
-              offset: Offset(0, 3),
-            ),
-          ],
-        ),
-        child: Row(
-          children: [
-            Container(
-              width: 80,
-              height: 80,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8.0),
-                border: Border.all(
-                  color: Colors.white,
-                  width: 1.0,
-                ),
-                image: DecorationImage(
-                  image: AssetImage(imageUrl),
-                  fit: BoxFit.cover,
+      child: Hero(
+        tag: eventTitle,
+        child: Container(
+          height: 130,
+          margin: EdgeInsets.symmetric(horizontal: 16.0),
+          padding: EdgeInsets.all(16.0),
+          decoration: BoxDecoration(
+            color: Color(0xFF000B45),
+            borderRadius: BorderRadius.circular(16.0),
+            boxShadow: [
+              BoxShadow(
+                color: Color.fromARGB(255, 0, 0, 0).withOpacity(0.2),
+                spreadRadius: 5,
+                blurRadius: 2,
+                offset: Offset(0, 3),
+              ),
+            ],
+          ),
+          child: Row(
+            children: [
+              Container(
+                width: 80,
+                height: 80,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8.0),
+                  border: Border.all(
+                    color: Colors.white,
+                    width: 1.0,
+                  ),
+                  image: DecorationImage(
+                    image: AssetImage(imageUrl),
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
-            ),
-            SizedBox(width: 8.0),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 8.0),
-                    child: Text(
-                      eventTitle,
-                      style: TextStyle(
-                        fontSize: 16.0,
-                        fontFamily: 'Montserrat',
-                        fontWeight: FontWeight.w600,
-                        color: Colors.white,
+              SizedBox(width: 8.0),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 8.0),
+                      child: Text(
+                        eventTitle,
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 8.0),
-                    child: Text(
-                      userName,
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 8.0),
+                      child: Text(
+                        userName,
+                        style: TextStyle(
+                          fontSize: 12.0,
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.w400,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      dateAndTime,
                       style: TextStyle(
                         fontSize: 12.0,
                         fontFamily: 'Montserrat',
@@ -227,29 +101,20 @@ class EventCard extends StatelessWidget {
                         color: Colors.white,
                       ),
                     ),
-                  ),
-                  Text(
-                    dateAndTime,
-                    style: TextStyle(
-                      fontSize: 12.0,
-                      fontFamily: 'Montserrat',
-                      fontWeight: FontWeight.w400,
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
 
   void _showEventDetails(BuildContext context) {
-    PageRouteBuilder<dynamic> route = PageRouteBuilder(
-      pageBuilder: (BuildContext context, Animation<double> animation,
-          Animation<double> secondaryAnimation) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
         return Dialog(
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(16.0),
@@ -318,7 +183,136 @@ class EventCard extends StatelessWidget {
                         ),
                       ),
                     ),
-                    // Adicione o restante do conteúdo conforme necessário
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: RichText(
+                        text: TextSpan(
+                          style: TextStyle(
+                            fontSize: 12.0,
+                            fontFamily: 'Montserrat',
+                            color: Colors.white,
+                          ),
+                          children: [
+                            TextSpan(
+                              text: "Data / Hora: ",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            TextSpan(
+                              text: "$dateAndTime",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: RichText(
+                        text: TextSpan(
+                          style: TextStyle(
+                            fontSize: 12.0,
+                            fontFamily: 'Montserrat',
+                            color: Colors.white,
+                          ),
+                          children: [
+                            TextSpan(
+                              text: "Discord account: ",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            TextSpan(
+                              text: "$discordAccount",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: RichText(
+                        text: TextSpan(
+                          style: TextStyle(
+                            fontSize: 12.0,
+                            fontFamily: 'Montserrat',
+                            color: Colors.white,
+                          ),
+                          children: [
+                            TextSpan(
+                              text: "Nome do jogo: ",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            TextSpan(
+                              text: "$gameName",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: RichText(
+                        text: TextSpan(
+                          style: TextStyle(
+                            fontSize: 12.0,
+                            fontFamily: 'Montserrat',
+                            color: Colors.white,
+                          ),
+                          children: [
+                            TextSpan(
+                              text: "Idade limite: ",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            TextSpan(
+                              text: "$ageLimit",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: RichText(
+                        text: TextSpan(
+                          style: TextStyle(
+                            fontSize: 12.0,
+                            fontFamily: 'Montserrat',
+                            color: Colors.white,
+                          ),
+                          children: [
+                            TextSpan(
+                              text: "Descrição: ",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            TextSpan(
+                              text: "$description",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
                   ],
                 ),
               ),
@@ -339,26 +333,7 @@ class EventCard extends StatelessWidget {
           ),
         );
       },
-      transitionsBuilder: (BuildContext context, Animation<double> animation,
-          Animation<double> secondaryAnimation, Widget child) {
-        const begin = 0.0;
-        const end = 1.0;
-        const curve = Curves.easeInOut;
-        var fadeTween =
-            Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-        var fadeAnimation = animation.drive(fadeTween);
-        return FadeTransition(
-          opacity: fadeAnimation,
-          child: Container(
-            color: Colors.black
-                .withOpacity(0.5), // Ajuste a opacidade conforme necessário
-            child: child,
-          ),
-        );
-      },
-      transitionDuration: const Duration(milliseconds: 300),
     );
-    Navigator.of(context).push(route);
   }
 }
 
